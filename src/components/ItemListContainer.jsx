d33ceed55f8b37b741345dd6e7f77636357ea090
Pablo Montenegro <montenegrop681@gmail.com>
2025-11-24 21:46:19 -0300

    CreaTuLanding+Montenegro

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,7 +3,7 @@ const ItemListContainer = ({ greeting }) => {
       <main className="item-list-container">
         <h1 className="item-list-container__title">{greeting}</h1>
         <p className="item-list-container__subtitle">
-          Muy pronto vas a poder ver el catálogo completo de productos alimenticios de Lo de Mané.
+          Muy pronto vas a poder ver el catálogo completo de productos alimenticios y de limpieza.
         </p>
       </main>
     );
